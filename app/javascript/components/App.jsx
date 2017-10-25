5fcca731bd627c90385ab7fa3a1f595c80ff552d
JONATHAN <jonn.thinkrtc@gmail.com>
2017-10-24 18:32:10 -0700

    first config

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+	render(){
+		return(
+			<div className="app-component-title">
+				<h1>SuperSquad</h1>
+			</div>
+			)
+
+	}
+}
+
+
+export default App;
