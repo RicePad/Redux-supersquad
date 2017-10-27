1059edaba81c3da3126a5cc28e86618dca327316
JONATHAN <jonn.thinkrtc@gmail.com>
2017-10-26 20:02:30 -0700

    updated background image

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
 	render(){
 		return(
 			<div className="App">
-				<h1>SuperSquad</h1>
+				<h1>RicePad Squad</h1>
 				
 				<div className="row">
 					<div className="col-md-4">
